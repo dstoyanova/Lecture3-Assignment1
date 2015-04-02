2cfba9f9565400769115ae13a1261bddaece100d
Desislava Stoyanova <stoyanova@Desislavas-MacBook-Air.local>
2015-04-03 00:05:54 +0300

    Initial Commit

---
+++ Lecture3-1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Lecture3-1
+//
+//  Created by Desislava Stoyanova on 4/3/15.
+//  Copyright (c) 2015 Desislava Stoyanova. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Lecture3-1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Lecture3-1
+//
+//  Created by Desislava Stoyanova on 4/3/15.
+//  Copyright (c) 2015 Desislava Stoyanova. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
